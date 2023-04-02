50cd8eaa846b20d1732b96bab7bdc5f30ef6ef57
Sébastien Doeraene <sjrdoeraene@gmail.com>
2023-04-02 14:12:27 +0200

    Fix the ComponentID.materializeID implementation.

--- core/src/main/scala/com/funlabyrinthe/core/ComponentID.scala
+++ core/src/main/scala/com/funlabyrinthe/core/ComponentID.scala
@@ -14,8 +14,16 @@ object ComponentID {
   def materializeIDImpl(using Quotes): Expr[ComponentID] =
     import quotes.reflect.*
 
-    val name = Symbol.spliceOwner.name
-    val nameExpr = Literal(StringConstant(name)).asExprOf[String]
-    '{ new ComponentID($nameExpr) }
+    def findGoodOwner(owner: Symbol): Option[Symbol] =
+      if !owner.exists then None
+      else if owner.isValDef then Some(owner)
+      else findGoodOwner(owner.maybeOwner)
+
+    findGoodOwner(Symbol.spliceOwner.maybeOwner) match
+      case Some(owner) =>
+        val nameExpr = Literal(StringConstant(owner.name)).asExprOf[String]
+        '{ new ComponentID($nameExpr) }
+      case None =>
+        report.errorAndAbort("Cannot automatically materialize a Component ID here. Did you assign to a `val`?")
   end materializeIDImpl
 }
