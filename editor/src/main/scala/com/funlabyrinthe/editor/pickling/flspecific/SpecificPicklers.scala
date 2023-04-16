20232f1b5ea8bfd7c7ef6993627990615efd2fe5
Sébastien Doeraene <sjrdoeraene@gmail.com>
2023-04-16 17:02:46 +0200

    Remove UniversePickler and ResourceLoaderPickler.
    Values of these types should be immutable and already initialized
    with the correct global instances, so there is no point in pickling
    them at all.

--- editor/src/main/scala/com/funlabyrinthe/editor/pickling/flspecific/SpecificPicklers.scala
+++ editor/src/main/scala/com/funlabyrinthe/editor/pickling/flspecific/SpecificPicklers.scala
@@ -11,36 +11,9 @@ class SpecificPicklers(val universe: Universe) {
   import RegistryEntry.{ ExactType, SubType, _ }
 
   def registerSpecificPicklers(registry: PicklingRegistry): Unit = {
-    registry.registerSubType(
-        InspectedType.staticMonoClass[Universe], (_, _) => UniversePickler, 95)
-    registry.registerSubType(
-        InspectedType.staticMonoClass[ResourceLoader], (_, _) => ResourceLoaderPickler, 95)
-
     registry.registerSubTypeReadWrite(
         InspectedType.staticMonoClass[Component], (_, _) => ComponentRefPickler, 95)
   }
-
-  abstract class GlobalPickler(global: AnyRef, fakeString: String) extends Pickler {
-    def pickle(data: InspectedData)(implicit ctx: Context): Pickle = {
-      assert(data.value == global, s"Trying to pickle another $fakeString")
-      StringPickle(fakeString)
-    }
-
-    def unpickle(data: InspectedData, pickle: Pickle)(
-        implicit ctx: Context): Unit = {
-      pickle match {
-        case StringPickle(`fakeString`) if !data.isReadOnly =>
-          data.asWritable.value = global
-        case _ => ()
-      }
-    }
-  }
-
-  object UniversePickler extends GlobalPickler(
-      universe, "<universe>")
-
-  object ResourceLoaderPickler extends GlobalPickler(
-      universe.resourceLoader, "<resource-loader>")
 
   object ComponentRefPickler extends Pickler {
     def pickle(data: InspectedData)(implicit ctx: Context): Pickle = {
