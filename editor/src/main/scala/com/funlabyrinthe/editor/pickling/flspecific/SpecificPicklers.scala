8195d4142d1450bff8cbf95c51d7d082614a2c48
Sébastien Doeraene <sjrdoeraene@gmail.com>
2023-04-16 17:11:33 +0200

    Use registerPickleable for the ComponentRef pickler.

--- editor/src/main/scala/com/funlabyrinthe/editor/pickling/flspecific/SpecificPicklers.scala
+++ editor/src/main/scala/com/funlabyrinthe/editor/pickling/flspecific/SpecificPicklers.scala
@@ -1,33 +1,29 @@
 package com.funlabyrinthe.editor.pickling
 package flspecific
 
+import com.funlabyrinthe.core._
 import com.funlabyrinthe.core.reflect._
+
 import com.funlabyrinthe.editor.reflect._
 
-import com.funlabyrinthe.core.{ Universe, _ }
-
 class SpecificPicklers(val universe: Universe) {
-  import TypeDirectedRegistry.Entry._
-  import RegistryEntry.{ ExactType, SubType, _ }
-
   def registerSpecificPicklers(registry: PicklingRegistry): Unit = {
-    registry.registerSubTypeReadWrite(
-        InspectedType.staticMonoClass[Component], (_, _) => ComponentRefPickler, 95)
+    registry.registerPickleable[Component]()
   }
 
-  object ComponentRefPickler extends Pickler {
-    def pickle(data: InspectedData)(implicit ctx: Context): Pickle = {
-      val component = data.value.asInstanceOf[Component]
+  given ComponentRefPickleable: Pickleable[Component] with
+    def pickle(value: Component)(using Context): Pickle = {
+      val component = value.asInstanceOf[Component]
       StringPickle(component.id)
     }
 
-    def unpickle(data: InspectedData, pickle: Pickle)(
-        implicit ctx: Context): Unit = {
+    def unpickle(pickle: Pickle)(using Context): Option[Component] = {
       pickle match {
         case StringPickle(id) =>
-          data.asWritable.value = universe.getComponentByID(id)
-        case _ => ()
+          Some(universe.getComponentByID(id))
+        case _ =>
+          None
       }
     }
-  }
+  end ComponentRefPickleable
 }
