aa8600f49f4748bb3978b1f86184592e9700746c
Sébastien Doeraene <sjrdoeraene@gmail.com>
2023-08-13 23:44:36 +0200

    Remove demo text from the renderer.

--- editor-renderer/src/main/scala/com/funlabyrinthe/editor/renderer/Renderer.scala
+++ editor-renderer/src/main/scala/com/funlabyrinthe/editor/renderer/Renderer.scala
@@ -14,14 +14,6 @@ import com.funlabyrinthe.graphics.html as ghtml
 import com.funlabyrinthe.mazes.*
 
 object Renderer:
-  @js.native
-  @JSGlobal
-  object versions extends js.Object {
-    def node(): String = js.native
-    def chrome(): String = js.native
-    def electron(): String = js.native
-  }
-
   def main(args: Array[String]): Unit =
     renderOnDomContentLoaded(dom.document.body, new Renderer().appElement)
 end Renderer
@@ -30,16 +22,10 @@ class Renderer:
   import Renderer.*
 
   val appElement: Element =
-    import versions.*
     val universe = makeUniverse()
     val universeFile = new UniverseFile(universe)
     val universeEditor = new UniverseEditor(universeFile)
-    div(
-      h1("Hello from Electron renderer with Laminar!"),
-      p("👋"),
-      p(s"This app is using Chrome (v${chrome()}), Node.js (v${node()}), and Electron (v${electron()})"),
-      universeEditor.topElement,
-    )
+    universeEditor.topElement
   end appElement
 
   private def makeUniverse(): Universe =
