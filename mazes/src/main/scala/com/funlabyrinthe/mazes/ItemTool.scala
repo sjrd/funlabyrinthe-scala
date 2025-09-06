c51de69c43652ef505b4ad009eb6108721eac553
Sébastien Doeraene <sjrdoeraene@gmail.com>
2025-09-06 17:34:58 +0200

    Enable reflection over ItemTool.

--- mazes/src/main/scala/com/funlabyrinthe/mazes/ItemTool.scala
+++ mazes/src/main/scala/com/funlabyrinthe/mazes/ItemTool.scala
@@ -3,11 +3,13 @@ package com.funlabyrinthe.mazes
 import com.funlabyrinthe.core.*
 import com.funlabyrinthe.mazes.std.*
 
-class ItemTool(using ComponentInit) extends Tool {
+class ItemTool(using ComponentInit) extends Tool derives Reflector {
   var item: Option[ItemDef] = None
 
   var count: Int = 1
   var message: String = ""
+
+  override def reflect() = autoReflect[ItemTool]
 
   override def find(context: MoveContext): Unit = {
     import context._
