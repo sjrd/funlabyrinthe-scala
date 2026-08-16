18a1e90dff0628eade345afc3c92d106121c4af8
Sébastien Doeraene <sjrdoeraene@gmail.com>
2026-08-16 23:52:17 +0200

    Allow to create an EnteredContext with an explicit position.

--- mazes/src/main/scala/com/funlabyrinthe/mazes/EnteredContext.scala
+++ mazes/src/main/scala/com/funlabyrinthe/mazes/EnteredContext.scala
@@ -2,12 +2,15 @@ package com.funlabyrinthe.mazes
 
 import com.funlabyrinthe.core.*
 
-class EnteredContext(val player: Player, val optSrc: Option[SquareRef])
+class EnteredContext(val player: Player, val pos: SquareRef, val optSrc: Option[SquareRef])
     extends AbstractMoveContext {
 
-  require(player.position.isDefined, s"Missing player position")
+  def this(player: Player, optSrc: Option[SquareRef]) = {
+    this(player, player.position.getOrElse {
+      throw IllegalArgumentException(s"Missing player position")
+    }, optSrc)
+  }
 
-  val pos = player.position.get
   val map: Map = pos.map
 
   var goOnMoving = false
